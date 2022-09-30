081619a96c5209d391569cb3a965d4463f72a08a
raauf <vrairaaufabidi@gmail.com>
2022-10-01 00:27:48 +0200

    after solved insertNodeAtTail problem in hackerrank i add it in commenter

--- LinkedLists.java
+++ LinkedLists.java
@@ -244,3 +244,22 @@ static void printLinkedList(SinglyLinkedListNode head) {
             }
     }
 */
+/*
+
+insert node at the end  of liste solving in hackerrank
+static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        SinglyLinkedListNode node=new SinglyLinkedListNode(data);
+        if(head==null)
+        {
+            head=node;
+            return head;
+        }
+        SinglyLinkedListNode current=head;
+        while(current.next!=null){
+            current=current.next;
+        }
+        current.next=node;
+        return head;
+
+    }
+*/
